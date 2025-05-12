1aede34f580daa0f7e6386fdd190633f8e5b3a80
orizuro <alexandre.plotean@gmail.com>
2025-05-12 23:45:16 +0100

    front

--- PC2/php/app.py
+++ PC2/php/app.py
@@ -1,13 +1,14 @@
-from flask import Flask, jsonify
-from routes.html import web
+from flask import Flask
+from routes.web import web
 from routes.android import android
+
 # ------------------ FLASK APP ------------------ #
 app = Flask(__name__)
 
 app.secret_key = "your_secret_key"
 app.register_blueprint(web, url_prefix='/scripts/web')
-app.register_blueprint(android, url_prefix='/scripts/android')
+app.register_blueprint(android, url_prefix='/scripts/php')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run( host='0.0.0.0', port=5000)
